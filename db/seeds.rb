9fff03bfb01cb6c8450ccacd43ca969bb29b9fdc
Eric Chen <eric19920108@gmail.com>
2020-09-02 15:05:27 +0800

    day33

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+5.times do
+    restaurant = Restaurant.new(
+       name: ['Restaurant1', 'Restaurant2', 'Restaurant3'].sample,
+       phone_number: ['123456789', '0987654321', '234567853'].sample,
+       address: ['Melbourne', 'Taipei', 'Kyoto'].sample,
+       category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+    restaurant.save!
+
+    review = Review.new(
+        restaurant: restaurant,
+        content: ['great', 'good', 'soso', 'bad'].sample,
+        rating: rand(0..5)
+    )
+    review.save!
+end
+
+puts "Finished!"
+
